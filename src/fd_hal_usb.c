3a8ba4a0fd567e1cbcc2a81067fbad4faab75b83
Denis Bohm <denis@fireflydesign.com>
2014-09-08 18:20:10 -0500

    add hal function to get usb vendor and product ids

--- src/fd_hal_usb.c
+++ src/fd_hal_usb.c
@@ -1,6 +1,14 @@
 #include "fd_hal_usb.h"
 #include "fd_usb.h"
 
+uint16_t fd_hal_usb_get_vendor_id(void) {
+    return fd_usb_get_vendor_id();
+}
+
+uint16_t fd_hal_usb_get_product_id(void) {
+    return fd_usb_get_product_id();
+}
+
 bool fd_hal_usb_is_connected(void) {
     return fd_usb_is_connected();
 }
--- src/fd_hal_usb.h
+++ src/fd_hal_usb.h
@@ -2,6 +2,10 @@
 #define FD_HAL_USB_H
 
 #include <stdbool.h>
+#include <stdint.h>
+
+uint16_t fd_hal_usb_get_vendor_id(void);
+uint16_t fd_hal_usb_get_product_id(void);
 
 bool fd_hal_usb_is_connected(void);
 bool fd_hal_usb_is_powered(void);

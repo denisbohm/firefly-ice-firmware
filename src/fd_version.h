c1bfac9e4815a31514cd329ef754a2399a44bcc3
Denis Bohm <denis@fireflydesign.com>
2014-11-21 15:53:45 -0600

    add version file

---
+++ src/fd_version.h
@@ -0,0 +1,34 @@
+#ifndef FD_VERSION_H
+#define FD_VERSION_H
+
+#include <stdint.h>
+
+#define FD_VERSION_METADATA_MAGIC 0xb001da1a
+
+#define FD_VERSION_COMMIT_SIZE 20
+
+typedef struct {
+    uint16_t major;
+    uint16_t minor;
+    uint16_t patch;
+    uint32_t capabilities;
+    uint8_t commit[FD_VERSION_COMMIT_SIZE];
+} fd_version_t;
+
+typedef struct {
+    uint32_t length;
+    uint8_t hash[FD_SHA_HASH_SIZE];
+} fd_version_binary_t;
+
+typedef struct {
+    uint32_t magic;
+    fd_version_t version;
+    fd_version_binary_t binary;
+} fd_version_metadata_t;
+
+typedef struct {
+    uint16_t major;
+    uint16_t minor;
+} fd_version_hardware_t;
+
+#endif

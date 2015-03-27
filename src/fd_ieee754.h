82ba73557d5b3a5949d362794311a914789bb9f7
Denis Bohm <denis@fireflydesign.com>
2015-03-27 13:03:16 -0500

    add more type conversions

--- src/fd_ieee754.h
+++ src/fd_ieee754.h
@@ -3,6 +3,9 @@
 
 #include <stdint.h>
 
+float fd_ieee754_uint32_to_float(uint32_t value);
+uint32_t fd_ieee754_float_to_uint32(float value);
+
 uint16_t fd_ieee754_float_to_uint16(float value);
 float fd_ieee754_uint16_to_float(uint16_t value);
 
--- src/fd_ieee754_vfp.c
+++ src/fd_ieee754_vfp.c
@@ -1,5 +1,22 @@
 #include "fd_ieee754.h"
 
+typedef union {
+    uint32_t as_uint32;
+    float as_float32;
+} fd_int32_float32_t;
+
+float fd_ieee754_uint32_to_float(uint32_t bfp) {
+    fd_int32_float32_t u;
+    u.as_uint32 = bfp;
+    return u.as_float32;
+}
+
+uint32_t fd_ieee754_float_to_uint32(float sfp) {
+    fd_int32_float32_t u;
+    u.as_float32 = sfp;
+    return u.as_uint32;
+}
+
 float fd_ieee754_uint16_to_float(uint16_t hfp) {
     float sfp;
     __asm(

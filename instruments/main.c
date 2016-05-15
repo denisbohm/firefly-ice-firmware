0812d684ecaf730567f5de24cb9d61762b31b879
Denis Bohm <denis@fireflydesign.com>
2016-05-15 17:39:46 -0500

    leds and relays working

--- instruments/main.c
+++ instruments/main.c
@@ -17,18 +17,39 @@ static void fdi_led_initialize(void) {
     GPIOC->MODER |= 0b01 << (FDI_LED_PIN_R * 2);
 }
 
-static void fdi_led_on(uint32_t led) {
-    GPIOC->BSRR = (1 << 16) << led;
+static void fdi_led_on(uint32_t pin) {
+    GPIOC->BSRR = (1 << 16) << pin;
 }
 
-static void fdi_led_off(uint32_t led) {
-    GPIOC->BSRR = 1 << led;
+static void fdi_led_off(uint32_t pin) {
+    GPIOC->BSRR = 1 << pin;
+}
+
+#define FDI_ATE_MCU_VCC_SENSE_PORT D
+#define FDI_ATE_MCU_VCC_SENSE_PIN 2
+
+static void fdi_relay_initialize(void) {
+    RCC->AHB1ENR |= 1 << 3; // enable GPIOD clock
+     // configure relay pins as outputs (initialized high)
+    GPIOD->BSRR = (1 << 16) << FDI_ATE_MCU_VCC_SENSE_PIN;
+    GPIOD->MODER |= 0b01 << (FDI_ATE_MCU_VCC_SENSE_PIN * 2);
+}
+
+static void fdi_relay_on(uint32_t pin) {
+    GPIOD->BSRR = 1 << pin;
+}
+
+static void fdi_relay_off(uint32_t pin) {
+    GPIOD->BSRR = (1 << 16) << pin;
 }
 
 void main(void) {
     fdi_led_initialize();
+    fdi_relay_initialize();
     fdi_led_on(FDI_LED_PIN_R);
     fdi_led_off(FDI_LED_PIN_R);
+    fdi_relay_on(FDI_ATE_MCU_VCC_SENSE_PIN);
+    fdi_relay_off(FDI_ATE_MCU_VCC_SENSE_PIN);
     fdi_led_on(FDI_LED_PIN_G);
     fdi_led_off(FDI_LED_PIN_G);
     fdi_led_on(FDI_LED_PIN_B);

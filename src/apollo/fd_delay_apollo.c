63ed0c606e8ba0e5ad7a91628ce879a6e379e451
Denis Bohm <denis@fireflydesign.com>
2018-04-23 20:13:27 -0500

    start of apollo utilities

---
+++ src/apollo/fd_delay_apollo.c
@@ -0,0 +1,25 @@
+void fd_delay_3x_cycles(uint32_t cycles) __attribute__((naked, used));
+
+void fd_delay_3x_cycles(uint32_t cycles __attribute__((unused))) {
+    __asm(
+        "    subs r0, #1\n"
+        "    bne fd_delay_3x_cycles\n"
+        "    bx lr"
+    );
+}
+
+#define CYCLES_PER_SECOND 48000000
+
+void fd_delay_ns(uint32_t ns) {
+    fd_delay_3x_cycles((us * CYCLES_PER_SECOND) / 3000000000);
+}
+
+void fd_delay_us(uint32_t us) {
+    fd_delay_3x_cycles((us * CYCLES_PER_SECOND) / 3000000);
+}
+
+void fd_delay_ms(uint32_t ms) {
+    while (ms--) {
+        fd_delay_3x_cycles(CYCLES_PER_SECOND / 3000);
+    }
+}

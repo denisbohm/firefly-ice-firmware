bdac0e641966d4a5e79ee36924b7fe219e48c55d
Denis Bohm <denis@fireflydesign.com>
2018-06-05 13:13:59 -0500

    quiescent test fixes

--- fixture/fd_quiescent_test_apollo.c
+++ fixture/fd_quiescent_test_apollo.c
@@ -1,12 +1,32 @@
 #include "fd_quiescent_test.h"
 
+#include "am_mcu_apollo.h"
 #include "mcu.h"
 
 #include <stdbool.h>
 #include <stdint.h>
 
 void fd_quiescent_test(void) {
+    // Enable internal buck converters.
+    am_hal_pwrctrl_bucks_init();
+ 
+    // Initialize for low power in the power control block
+    am_hal_pwrctrl_low_power_init();
+ 
+    // Turn off the voltage comparator as this is enabled on reset.
+    am_hal_vcomp_disable();
+ 
+    // Run the RTC off the LFRC (so XT will not be automatically started)
+    am_hal_rtc_osc_select(AM_HAL_RTC_OSC_LFRC);
+ 
+    // Stop the XT if crystal is not populated
+    am_hal_clkgen_osc_stop(AM_HAL_CLKGEN_OSC_XT);
+ 
+    // Disable the RTC if not using
+    am_hal_rtc_osc_disable();
+
     while (true) {
+        am_hal_sysctrl_sleep(AM_HAL_SYSCTRL_SLEEP_DEEP);
         __WFI();
     }
 }
--- fixture/fd_quiescent_test_nrf5.c
+++ fixture/fd_quiescent_test_nrf5.c
@@ -5,6 +5,7 @@
 #include "fd_gpio.h"
 #include "fd_i2cm.h"
 #include "fd_lsm6dsl.h"
+#include "fd_pwm.h"
 #include "fd_spi_flash.h"
 #include "fd_spim.h"
 
@@ -13,7 +14,27 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+void fd_quiescent_test_button_initialize(void) {
+    fd_gpio_t buttons[] = {
+        { .port = 1, .pin = 11 },
+        { .port = 1, .pin =  0 },
+        { .port = 0, .pin = 26 },
+        { .port = 1, .pin =  8 },
+        { .port = 0, .pin = 27 },
+    };
+    fd_gpio_configure_input_pull_up(buttons[0]);
+    const int count = sizeof(buttons) / sizeof(buttons[0]);
+    for (int i = 1; i < count; ++i) {
+        fd_gpio_configure_input_pull_up(buttons[i]);
+    }
+}
+
+float battery_voltage = 0.0f;
+
 void fd_quiescent_test_set_system_voltage(void) {
+    // wait for BQ25120A startup
+    fd_delay_ms(1000); // !!! 70 works, 65 does not.  TI says wait for twait1 min/0.08s, max/1.0s. -denis
+
     fd_i2cm_bus_t buses[] = {
         {
             .instance = (uint32_t)NRF_TWIM0,
@@ -31,10 +52,26 @@ void fd_quiescent_test_set_system_voltage(void) {
     
     fd_i2cm_initialize(buses, 1, devices, 1);
     fd_i2cm_bus_enable(bus);
+
+    fd_gpio_t cdn = { .port = 1, .pin = 15 };
+    fd_gpio_configure_output(cdn);
+    fd_gpio_set(cdn, true);
+    fd_delay_us(100);
+
     float voltage = 3.2f;
-    fd_bq25120_set_system_voltage(device, voltage);
+    bool result = fd_bq25120_set_system_voltage(device, voltage);
+    fd_delay_us(100);
+
+    result = fd_bq25120_read_battery_voltage(device, &battery_voltage);
+
+    fd_gpio_set(cdn, false);
+
     fd_i2cm_bus_disable(bus);
 }
+
+fd_spi_flash_information_t information;
+uint8_t who_am_i;
+uint8_t ctrl1_xl;
 
 void fd_quiescent_test_spi_initialize(void) {
     fd_spim_bus_t buses[] = {
@@ -69,13 +106,19 @@ void fd_quiescent_test_spi_initialize(void) {
     
     fd_spim_initialize(buses, bus_count, devices, device_count);
 
+#if 1
     fd_spim_bus_enable(flash_bus);
     uint32_t wake_delay_us = 30;
     fd_spi_flash_wake(flash_device, 30);
+    //
+    fd_spi_flash_get_information(flash_device, &information);
+    //
     fd_spi_flash_sleep(flash_device);
     fd_spim_bus_disable(flash_bus);
+#endif
 
     fd_spim_bus_enable(lsm6dsl_bus);
+#if 1
     fd_lsm6dsl_configuration_t configuration = {
         .fifo_threshold = 32,
         .fifo_output_data_rate = FD_LSM6DSL_ODR_13_HZ,
@@ -91,16 +134,81 @@ void fd_quiescent_test_spi_initialize(void) {
         .gyro_enable = false
     };
     fd_lsm6ds3_configure(lsm6dsl_device, &configuration);
+#else
+    fd_lsm6dsl_write(lsm6dsl_device, FD_LSM6DSL_REGISTER_CTRL3_C, 0b01010100); // block data update, int1/2 open drain, address automatically incremented
+#endif
+
+    //
+    who_am_i = fd_lsm6dsl_read(lsm6dsl_device, FD_LSM6DSL_REGISTER_WHO_AM_I);
+    ctrl1_xl = fd_lsm6dsl_read(lsm6dsl_device, FD_LSM6DSL_REGISTER_CTRL1_XL);
+    //
     uint32_t disable_delay = 100;
     fd_delay_us(disable_delay);
     fd_spim_bus_disable(lsm6dsl_bus);
 }
 
+void fd_quiescent_vibrate(void) {
+    const fd_pwm_module_t aw_pwm_modules[] = {
+        {
+            .instance = 0x4001C000, // NRF_PWM0
+            .frequency = 32000.0f
+        },
+    };
+    const fd_pwm_channel_t aw_pwm_channels[] = {
+        {
+            .module = &aw_pwm_modules[0],
+            .instance = 0,
+            .gpio = {
+                .port = 0,
+                .pin = 20
+            }
+        },
+    };
+    fd_pwm_initialize(aw_pwm_modules, 1);
+    fd_pwm_module_enable(&aw_pwm_modules[0]);
+    fd_pwm_channel_start(&aw_pwm_channels[0], 0.5f);
+}
+
+uint32_t loop_count = 0;
+
 void fd_quiescent_test(void) {
+    fd_gpio_t nreset = { .port = 0, .pin = 18 };
+    fd_gpio_configure_input_pull_up(nreset);
+    fd_gpio_t cdn = { .port = 1, .pin = 15 };
+    fd_gpio_configure_output(cdn);
+    fd_gpio_set(cdn, false);
+    fd_gpio_t bq_npg = { .port = 1, .pin = 10 };
+    fd_gpio_configure_input_pull_up(bq_npg);
+    fd_gpio_t bq_int = { .port = 0, .pin = 3 };
+    fd_gpio_configure_input(bq_int);
+    fd_gpio_t bq_lsctrl = { .port = 1, .pin = 14 };
+    fd_gpio_configure_output(bq_lsctrl);
+    fd_gpio_set(bq_lsctrl, true);
+    fd_gpio_t v5_en = { .port = 0, .pin = 8 };
+    fd_gpio_configure_output(v5_en);
+    fd_gpio_set(v5_en, false);
+
+    fd_gpio_t motor_en = { .port = 0, .pin = 20 };
+    fd_gpio_configure_output(motor_en);
+    fd_gpio_set(motor_en, false);
+
+    fd_gpio_t imu_int1 = { .port = 1, .pin = 6 };
+    fd_gpio_configure_input_pull_up(imu_int1);
+    fd_gpio_t imu_int2 = { .port = 1, .pin = 5 };
+    fd_gpio_configure_input_pull_up(imu_int2);
+
+    fd_quiescent_test_button_initialize();
     fd_quiescent_test_set_system_voltage();
     fd_quiescent_test_spi_initialize();
 
+    fd_quiescent_vibrate();
+
     while (true) {
-        __WFI();
+        ++loop_count;
+        // Wait for an event.
+        __WFE();
+        // Clear the internal event register.
+        __SEV();
+        __WFE();
     }
 }

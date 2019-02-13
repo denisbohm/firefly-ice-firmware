0d3412c93485e7b9898a3ed7a38ddc2fdad38725
Denis Bohm <denis@fireflydesign.com>
2019-02-12 19:39:01 -0600

    apollo rtos working

--- src/apollo/fd_rtos_apollo.c
+++ src/apollo/fd_rtos_apollo.c
@@ -6,7 +6,8 @@
 #include <string.h>
 
 typedef struct {
-    void *sp;
+    uint32_t stack_pointer;
+    uint32_t priority;
     bool runnable;
 } fd_rtos_task_t;
 
@@ -34,7 +35,11 @@ typedef struct {
 fd_rtos_task_t fd_rtos_tasks[fd_rtos_task_size];
 uint32_t fd_rtos_task_count;
 uint32_t fd_rtos_task_index;
+bool fd_rtos_task_start;
+uint32_t fd_rtos_delay_task_index;
 uint32_t fd_rtos_assertion_failure_count;
+
+uint8_t fd_rtos_sleep_stack[1024];
 
 void fd_rtos_assert(bool value) {
     if (!value) {
@@ -42,10 +47,19 @@ void fd_rtos_assert(bool value) {
     }
 }
 
+void fd_rtos_sleep_task(void) {
+    while (true) {
+        am_hal_sysctrl_sleep(AM_HAL_SYSCTRL_SLEEP_NORMAL);
+        fd_rtos_yield();
+    }
+}
+
 void fd_rtos_initialize(void) {
     memset(fd_rtos_tasks, 0, sizeof(fd_rtos_tasks));
     fd_rtos_task_count = 0;
     fd_rtos_task_index = 0;
+    fd_rtos_task_start = true;
+    fd_rtos_delay_task_index = 0;
     fd_rtos_assertion_failure_count = 0;
 
     NVIC_SetPriority(PendSV_IRQn, 1);
@@ -54,73 +68,93 @@ void fd_rtos_initialize(void) {
     NVIC_ClearPendingIRQ(STIMER_CMPR0_IRQn);
     NVIC_EnableIRQ(STIMER_CMPR0_IRQn);
     NVIC_SetPriority(STIMER_CMPR0_IRQn, 2);
+
+    fd_rtos_add_task(fd_rtos_sleep_task, fd_rtos_sleep_stack, sizeof(fd_rtos_sleep_stack), 0);
 }
 
 void fd_rtos_task_fallthrough(void) {
+    fd_rtos_assert(false);
     while (true) {
     }
 }
 
-void fd_rtos_add_task(fd_rtos_entry_point_t entry_point, void *stack, size_t stack_size) {
+void fd_rtos_add_task(fd_rtos_entry_point_t entry_point, void *stack, size_t stack_size, uint32_t priority) {
     if (fd_rtos_task_count >= fd_rtos_task_size) {
         return;
     }
     memset(stack, 0, stack_size);
-    fd_rtos_task_t *task = &fd_rtos_tasks[fd_rtos_task_index];
-    task->sp = ((uint8_t *)stack) + stack_size;
-    task->sp -= sizeof(fd_rtos_stack_frame_t);
-    fd_rtos_stack_frame_t *stack_frame = task->sp;
+    fd_rtos_task_t *task = &fd_rtos_tasks[fd_rtos_task_count];
+    task->stack_pointer = ((uint32_t)stack) + stack_size;
+    task->stack_pointer -= sizeof(fd_rtos_stack_frame_t);
+    fd_rtos_stack_frame_t *stack_frame = (fd_rtos_stack_frame_t *)task->stack_pointer;
     memset(stack_frame, 0, sizeof(stack_frame));
     stack_frame->hardware.lr = (uint32_t)fd_rtos_task_fallthrough;
     stack_frame->hardware.pc = (uint32_t)entry_point;
     stack_frame->hardware.psr = 0x21000000;
+    task->priority = priority;
     task->runnable = true;
-    ++fd_rtos_task_index;
+    ++fd_rtos_task_count;
 }
 
 // schedule exception handler
 
-void fd_rtos_reschedule(void) __attribute__((naked));
+void am_pendsv_isr(void) __attribute__((naked));
 
-void fd_rtos_reschedule(void) {
-    // verify current task uses PSP
-    uint32_t lr;
-    __asm("MOV r0, lr" : "=r" (lr));
-    fd_rtos_assert(lr & 0x0004);
+void am_pendsv_isr(void) {
+    if (fd_rtos_task_start) {
+        fd_rtos_task_start = false;
+    } else {
+        // verify current task uses PSP
+        uint32_t link_register;
+        __asm("MOV %[link_register], lr" : [link_register] "=r" (link_register));
+        fd_rtos_assert(link_register & 0x0004);
 
-    // suspend current task
-    fd_rtos_task_t *task = &fd_rtos_tasks[fd_rtos_task_index];
-    // push r4-r11 onto the program stack
-    void *psp;
-    __asm(
-        "MRS r0, psp\n"
-        "STMDB r0!, {r4-r11}\n"
-        "MSR psp, r0\n"
-        : "=r" (psp));
-    task->sp = psp;
+        // suspend current task
+        fd_rtos_task_t *task = &fd_rtos_tasks[fd_rtos_task_index];
+        // push r4-r11 onto the program stack
+        uint32_t stack_pointer;
+        __asm(
+            "MRS r0, psp\n"
+            "STMDB r0!, {r4-r11}\n"
+            "mov %[stack_pointer], r0\n"
+            : [stack_pointer] "=r" (stack_pointer));
+        task->stack_pointer = stack_pointer;
+    }
 
     // find new task to run
-    do {
-        if (++fd_rtos_task_index >= fd_rtos_task_count) {
-            fd_rtos_task_index = 0;
+    fd_rtos_task_index = 0;
+    fd_rtos_task_t *task = &fd_rtos_tasks[fd_rtos_task_index]; // sleep task is always runnable
+    for (uint32_t i = 1; i < fd_rtos_task_count; ++i) {
+        uint32_t index = fd_rtos_task_index + i;
+        if (index >= fd_rtos_task_count) {
+            index -= fd_rtos_task_count;
         }
-        task = &fd_rtos_tasks[fd_rtos_task_index];
-    } while (task->runnable);
+        fd_rtos_task_t *candidate = &fd_rtos_tasks[index];
+        if (candidate->runnable && (candidate->priority > task->priority)) {
+            task = candidate;
+            fd_rtos_task_index = index;
+        }
+    }
 
-    // resume current task
+    // resume task to run
     // pop r4-r11 from the program stack
-    psp = task->sp;
+    uint32_t stack_pointer = task->stack_pointer;
+    fd_rtos_assert((0x10000000 <= stack_pointer) && (stack_pointer <= 0x10040000));
     __asm(
+        "MOV r0, %[stack_pointer]\n"
         "LDMFD r0!, {r4-r11}\n"
         "MSR psp, r0\n"
-        : : "r" (psp));
+        : : [stack_pointer] "r" (stack_pointer));
 
     // return from this exception handler
     const uint32_t EXC_RETURN_THREAD_PSP = 0xfffffffd;
-    __asm("bx r0" : : "r"(EXC_RETURN_THREAD_PSP));
+    __asm("bx %[EXC_RETURN_THREAD_PSP]" : : [EXC_RETURN_THREAD_PSP] "r" (EXC_RETURN_THREAD_PSP));
 }
 
-void fd_rtos_start(void) {
+void fd_rtos_run(void) {
+    fd_rtos_assert(fd_rtos_task_start);
+    fd_rtos_yield();
+    fd_rtos_task_fallthrough();
 }
 
 void fd_rtos_yield(void) {
@@ -128,58 +162,42 @@ void fd_rtos_yield(void) {
     SCB->ICSR |= SCB_ICSR_PENDSVSET_Msk;
 }
 
-void STIMER_CMPR0_IRQHandler(void) {
+void am_stimer_cmpr0_isr(void) {
     am_hal_stimer_int_clear(AM_HAL_STIMER_INT_COMPAREA);
     am_hal_stimer_int_disable(AM_HAL_STIMER_INT_COMPAREA);
 
-    fd_rtos_task_t *task = &fd_rtos_tasks[fd_rtos_task_index];
+    fd_rtos_task_t *task = &fd_rtos_tasks[fd_rtos_delay_task_index];
     task->runnable = true;
-
-    // invoke scheduler exception handler
-    SCB->ICSR |= SCB_ICSR_PENDSVSET_Msk;
 }
 
 void fd_rtos_delay_delta(uint32_t delta) {
-    fd_rtos_task_t *task = &fd_rtos_tasks[fd_rtos_task_index];
+    fd_rtos_delay_task_index = fd_rtos_task_index;
+    fd_rtos_task_t *task = &fd_rtos_tasks[fd_rtos_delay_task_index];
     task->runnable = false;
     am_hal_stimer_int_enable(AM_HAL_STIMER_INT_COMPAREA);
     uint32_t compare_a = 0;
     am_hal_stimer_compare_delta_set(compare_a, delta);
 
-    // invoke scheduler exception handler
-    SCB->ICSR |= SCB_ICSR_PENDSVSET_Msk;
+    fd_rtos_yield();
 }
 
 // 3 mHz stimer clock & round up
 #define fd_rtos_delay(s) fd_rtos_delay_delta((uint32_t)((s) * 3000000.0) + 1)
 
-uint32_t main_work_count = 0;
+uint8_t fd_rtos_test_delay_stack[1024];
+uint32_t fd_rtos_test_delay_count = 0;
 
-void main_work_task(void) {
+void fd_rtos_test_delay_task(void) {
     while (true) {
-        uint32_t count = 0;
-        for (int i = 0; i < 1000; ++i) {
-            ++count;
-        }
-        ++main_work_count;
-        fd_rtos_yield();
-    }
-}
-
-uint32_t main_delay_count = 0;
-
-void main_delay_task(void) {
-    while (true) {
-        ++main_delay_count;
+        ++fd_rtos_test_delay_count;
         fd_rtos_delay(1.0);
     }
 }
 
 void fd_rtos_test(void) {
     fd_rtos_initialize();
-    uint8_t work_stack[1024];
-    fd_rtos_add_task(main_work_task, work_stack, sizeof(work_stack));
-    uint8_t delay_stack[1024];
-    fd_rtos_add_task(main_delay_task, delay_stack, sizeof(delay_stack));
-//    fd_rtos_run();
+
+    fd_rtos_add_task(fd_rtos_test_delay_task, fd_rtos_test_delay_stack, sizeof(fd_rtos_test_delay_stack), 1);
+
+    fd_rtos_run();
 }
--- src/fd_rtos.h
+++ src/fd_rtos.h
@@ -9,8 +9,12 @@ typedef void (*fd_rtos_entry_point_t)(void);
 
 void fd_rtos_initialize(void);
 
-void fd_rtos_add_task(fd_rtos_entry_point_t entry_point, void *stack, size_t stack_size);
+void fd_rtos_add_task(fd_rtos_entry_point_t entry_point, void *stack, size_t stack_size, uint32_t priority);
 
 void fd_rtos_yield(void);
+
+void fd_rtos_run(void);
+
+void fd_rtos_test(void);
 
 #endif

ce37d5d78d85d44ba9a191d494bba7e263e92259
Denis Bohm <denis@fireflydesign.com>
2019-12-17 18:00:34 -0600

    fix undefined return

--- src/nrf5/fd_ble_nrf5.c
+++ src/nrf5/fd_ble_nrf5.c
@@ -150,6 +150,7 @@ fd_ble_characteristic_nrf5_t *fd_ble_get_characteristic_nrf5(uint16_t uuid) {
             }
         }
     }
+    return 0;
 }
 
 bool fd_ble_set_characteristic_value(uint16_t uuid, uint8_t *value, uint32_t length) {
@@ -836,7 +837,7 @@ void fd_ble_soc_evt_handler(uint32_t event_id, void FD_UNUSED *p_context) {
 
 APP_TIMER_DEF(fd_ble_timer_id);
 
-void fd_ble_timer_handler(void *context) {
+void fd_ble_timer_handler(void *context FD_UNUSED) {
     if (fd_ble_configuration->on_tick) {
         fd_ble_configuration->on_tick();
     }
